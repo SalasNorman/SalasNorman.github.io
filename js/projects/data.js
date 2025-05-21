f0d07c7a45095d4372c8111e83ee44f1cf46c1b2
SalasNorman <salasnorman015@gmail.com>
2025-05-21 14:20:09 +0800

    add project

--- js/projects/data.js
+++ js/projects/data.js
@@ -6,9 +6,15 @@ const projects = [
     link: "https://github.com/SalasNorman/SalasNorman.github.io"
   },
   {
-    image: "assets/project2.jpg",
+    image: "assets/projects/simple_grading_system.webp",
     title: "Simple Grading System",
-    description: "",
+    description: "A simple VB.NET grading system using MS Access database. It has student records, clickable details, and weighted grade computation (Exam 50%, Project 20%, Quiz 10%, Class Participation 10%).",
     link: "https://github.com/SalasNorman/SimpleGradingSystem"
+  },
+    {
+    image: "assets/projects/simple_grading_system.webp",
+    title: "Basic Website using PHP",
+    description: "A simple website built with PHP (using PDO) and MySQL to showcase basic CRUD operations.",
+    link: "https://github.com/SalasNorman/Basic-Website-using-PHP"
   }
 ];
