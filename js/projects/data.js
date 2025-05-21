c15301fcb38ba611e9a54ee086fddc8b866cae5a
SalasNorman <salasnorman015@gmail.com>
2025-05-21 14:35:35 +0800

    update projects

--- js/projects/data.js
+++ js/projects/data.js
@@ -6,15 +6,15 @@ const projects = [
     link: "https://github.com/SalasNorman/SalasNorman.github.io"
   },
   {
+    image: "",
+    title: "Basic Website using PHP",
+    description: "TA simple phonebook application built using Flask with SQLite. It showcases basic CRUD (Create, Read, Update, Delete) operations for managing contacts.",
+    link: "https://github.com/SalasNorman/exercise101"
+  },
+    {
     image: "assets/projects/simple_grading_system.webp",
     title: "Simple Grading System",
     description: "A simple VB.NET grading system using MS Access database. It has student records, clickable details, and weighted grade computation (Exam 50%, Project 20%, Quiz 10%, Class Participation 10%).",
     link: "https://github.com/SalasNorman/SimpleGradingSystem"
-  },
-    {
-    image: "assets/projects/simple_grading_system.webp",
-    title: "Basic Website using PHP",
-    description: "A simple website built with PHP (using PDO) and MySQL to showcase basic CRUD operations.",
-    link: "https://github.com/SalasNorman/Basic-Website-using-PHP"
   }
 ];
